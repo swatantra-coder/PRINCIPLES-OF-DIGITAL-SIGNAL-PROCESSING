e98a1c1a48265980b014da223730de3d4ded95fa
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2022-02-28 15:09:05 +0530

    Update FFT.m

--- LAB5/FFT.m
+++ LAB5/FFT.m
@@ -4,11 +4,13 @@ close all;
 x=input("Enter the sequnce: ");
 N=length(x);
 X=zeros(1,N);
+tic
 for k=0:N-1
     for n=0:N-1
         X(k+1)=X(k+1)+x(n+1)*exp(-j*pi/2*n*k);
     end
 end
+toc
 %x1=input("Enter the second sequence: ");
 Y=fft(X)
 t=0:N-1;
